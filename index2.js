855bb5819f881210219860e1ef148d11528d4518
devmanoshri <manoshri.no@gmail.com>
2022-09-17 10:32:48 +0200

    redux practice

---
+++ index2.js
@@ -0,0 +1,36 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const BUY_CAKE = "BUY_CAKE";
+
+// action creator function
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+const initialSate = {
+  numOfCake: 10,
+};
+
+const reducer = (state = initialSate, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCake: state.numOfCake - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated state", store.getState());
+});
+store.dispatch(buyCake());
+unsubscribe();
